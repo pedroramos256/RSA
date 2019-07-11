2642fd90c3fcd3cdf11a946fa8600b2845d13d9d
pedroramos256 <pedrojgcramos@gmail.com>
2019-07-11 20:26:00 +0100

    second commit, comments added

--- RSA.c
+++ RSA.c
@@ -1,7 +1,9 @@
-#include <stdio.h>
-#include <math.h>
+#include <stdio.h>/*needed for printf and scanf */
+#include <math.h>/*needed for pow and log (use -lm when compiling) */
+
 
 int saunderson(int a, int b){
+    /*function used in RSA encryption */
 	int x0 = 1,y0 = 0,x1 = 0,y1 = 1,q,bubble;
 	while(a != 0 && b != 0){
 		q = a/b;
@@ -18,30 +20,42 @@ int saunderson(int a, int b){
 	return y0;
 }
 
-long int encrypt(int num,int n,int k){
+
+long int encrypt(int msg,int n,int k/*either "e" or "d"*/){
     /*notice the same function also decrypts*/
     int DIM = (int)(log((double)k)/log(2))+1,i;
-    long int v[DIM],R=1;
-    v[0]=num;
+    long int v[DIM],R = 1;/*remainder starts with the neutral value of multiplication */
+
+    v[0]=msg;/*since n is bigger than msg, pow(msg,1)%n = msg */
     for(i = 1;i < DIM;++i)
-        v[i]=(long int)(pow((double)v[i-1],2))%n;
-    for(i = DIM;i >= 0;--i)
-        if(k >= (int)pow(2,i)){
-            R*=v[i];
-            R%=n;
-            k-=(int)pow(2,i);
+        v[i] = (long int)(pow((double)v[i-1],2))%n;/*storing all remainders from pow(msg,2)%n to pow(msg,pow(2,DIM-1))%n */
+
+    for(i = DIM-1;i >= 0;--i){
+        /*calculates pow(msg,k)%n deconstructing k in powers of 2 */
+        int two_to_i = (int)pow(2,i);
+        if(k >= two_to_i){
+            R *= v[i];
+            R %= n;
+            k -= two_to_i;
         }
+    }
     return R;
 }
 
+
 int main(){
-	int p=61,q=47,N,e=17,d,NUM;
-	N = p*q;
-	d = (p-1)*(q-1)+saunderson((p-1)*(q-1),e);
-    scanf("%d",&NUM);
-    NUM = encrypt(NUM,N,e);
-    printf("message encrypted: %d\n",NUM);
-    NUM = encrypt(NUM,N,d);
-    printf("message decrypted: %d\n",NUM);
+	int p = 61,q = 47;/*two big primes and "e" smaller than lcm(p-1,q-1)*/
+    int N = p*q, e = 17;/*public key */
+	int d = (p-1)*(q-1)+saunderson((p-1)*(q-1),e);/*private key together with p and q */
+    int MSG;/*message must be less than N */
+
+    printf("Introduce an integer:\n");
+    scanf("%d",&MSG);
+
+    MSG = encrypt(MSG,N,e);/*encryption */
+    printf("message encrypted: %d\n",MSG);
+    
+    MSG = encrypt(MSG,N,d);/*decryption */
+    printf("message decrypted: %d\n",MSG);
 	return 0;
 }
